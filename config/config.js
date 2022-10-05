4950befe55420096059e9fd04c7da8b992e5089e
laraluitingh <lara.luitingh@gmail.com>
2022-10-06 10:56:11 +1100

    fixes

--- config/config.js
+++ config/config.js
@@ -13,6 +13,8 @@ const options = {
 };
 const pgp = require('pg-promise')(options);
 
+const proConfig = process.env.DATABASE_URL;
+
 
 const devConfig = `postgresql://${process.env.PG_USER}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`
 
