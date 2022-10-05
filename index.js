9bc98e19e1b2f22138488850fd578b52532ef59f
laraluitingh <lara.luitingh@gmail.com>
2022-10-05 16:31:41 +1100

    app . listen changes

--- index.js
+++ index.js
@@ -30,6 +30,4 @@ app.use((err, req, res, next) => {
 });
 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT || 5000);
